6448ee8b570f297808f68003199ebc6d991a2b22
Yeruva Sai Jahnavi <saijahnavi.yeruva@gmail.com>
2024-03-02 07:12:26 +0530

    Display case images using static files

--- brainiac/views/patient.py
+++ brainiac/views/patient.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for
 from database import db
 
 patient = Blueprint('patient', __name__,
@@ -8,7 +8,11 @@ patient = Blueprint('patient', __name__,
 @patient.route('/dashboard')
 def dashboard():
     patient_data = db.patients.find_one()
+
     if patient_data is None:
         return "No patient found", 404
 
-    return render_template('patient_dashboard.html', patient=patient_data)
+    # Define the paths to your images
+    image_paths = [url_for('static', filename='images/case-1.jpg'),
+                   url_for('static', filename='images/case-2.png')]
+    return render_template('patient_dashboard.html', patient=patient_data, image_paths=image_paths)
