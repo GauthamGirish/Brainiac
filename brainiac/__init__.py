0e36d48455f2fe95c17139f9d6295ab3ee196f9b
Yeruva Sai Jahnavi <saijahnavi.yeruva@gmail.com>
2024-03-02 06:26:35 +0530

    Connect to brainiac_db database

--- brainiac/__init__.py
+++ brainiac/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from dotenv import load_dotenv, find_dotenv
+import os
 
 
 def create_app():
     app = Flask(__name__)
+    # Load environment variables from .env file
+    load_dotenv(find_dotenv())
+
+    # Get the MongoDB URI from environment variables
+    app.config['MONGO_URI'] = os.getenv('MONGO_URI')
+    app.config['MONGO_URI'] = os.getenv('SECRET_KEY')
 
     # Import and register blueprints
     from .views.auth import auth
--- brainiac/views/patient.py
+++ brainiac/views/patient.py
@@ -1,7 +1,14 @@
 from flask import Blueprint, render_template
+from database import db
 
-patient = Blueprint('patient', __name__, template_folder='../templates/patient')
+patient = Blueprint('patient', __name__,
+                    template_folder='../templates/patient')
+
 
 @patient.route('/dashboard')
 def dashboard():
-    return render_template('patient_dashboard.html')
+    patient_data = db.patients.find_one()
+    if patient_data is None:
+        return "No patient found", 404
+
+    return render_template('patient_dashboard.html', patient=patient_data)
