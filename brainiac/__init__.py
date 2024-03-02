8feb3aebba37bcf83bae53b20735cd17df933ccb
Yeruva Sai Jahnavi <saijahnavi.yeruva@gmail.com>
2024-03-02 05:48:57 +0530

    Reset to code before connecting db

--- brainiac/__init__.py
+++ brainiac/__init__.py
@@ -1,19 +1,8 @@
 from flask import Flask
-from dotenv import load_dotenv
-import os
-from database import initialize_db  # Correct import
-
-load_dotenv()  # Load environment variables from the .env file
 
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-
-    # config and init db
-    app.config['MONGO_URI'] = os.getenv('MONGO_URI')
-    app.config['MONGO_DBNAME'] = os.getenv('MONGO_DBNAME')
-    initialize_db(app)
 
     # Import and register blueprints
     from .views.auth import auth
