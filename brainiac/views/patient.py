8b4aebae74a2114af4d6c385a3dff2cb5ee1871c
Yeruva Sai Jahnavi <saijahnavi.yeruva@gmail.com>
2024-03-03 11:14:26 +0530

    Rename patient_dashboard to patient_dash

--- brainiac/views/patient.py
+++ brainiac/views/patient.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, url_for, current_app
 
-patient_bp = Blueprint('patient', __name__ , url_prefix='/patient')
+patient_bp = Blueprint('patient', __name__, url_prefix='/patient')
+
 
 @patient_bp.route('/dashboard')
 def dashboard():
@@ -9,7 +10,7 @@ def dashboard():
     if patient_data is None:
         return "No patient found", 404
 
-    #Define the paths to your images
+    # Define the paths to your images
     image_paths = [url_for('static', filename='images/case-1.jpg'),
                    url_for('static', filename='images/case-2.png')]
     return render_template('patient_dash.html', patient=patient_data, image_paths=image_paths)
