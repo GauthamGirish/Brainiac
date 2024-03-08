b99d122a9870e62994d6eb4d7bc7b766c6b6d506
GauthamGirish <gautham7002@gmail.com>
2024-03-09 00:17:20 +0530

    Create model.py to handle integration

---
+++ brainiac/views/model.py
@@ -0,0 +1,8 @@
+import random
+
+def new_case(patient_data, case_name, img_urls):
+    
+    #pass the images into the model here
+    prediction = "random prediction"
+    accuracy = random.choice([25, 50, 75])
+    
