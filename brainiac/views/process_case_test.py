f7ec02d32b3defa76235afd038089e427e78f9a5
Yeruva Sai Jahnavi <saijahnavi.yeruva@gmail.com>
2024-04-24 17:16:45 +0530

    Testing new model accuracies

--- brainiac/views/process_case_test.py
+++ brainiac/views/process_case_test.py
@@ -4,10 +4,24 @@ from model_utils import load_model, preprocess_img_test, predict
 from collections import Counter
 
 # Load the model
-model_path = r'C:\Users\Jahnavi\Documents\3rd_Year\Y3S2\[C] CSE301\[0] Brainiac Repo\tumor_classifier_model.pth'
+model_path = r'C:\Users\Jahnavi\Documents\3rd_Year\Y3S2\[C] CSE301\[0] Brainiac Repo\tumor_classifier_model_improved.pth'
 model = load_model(model_path)
 
 # Define absolute paths to the test images
+# test_img_urls = [
+#     r"C:\Users\Jahnavi\Documents\3rd_Year\Y3S2\[C] CSE301\[0] Brainiac Repo\brainiac\static\images\Te-gl_0010.jpg",
+#     r"C:\Users\Jahnavi\Documents\3rd_Year\Y3S2\[C] CSE301\[0] Brainiac Repo\brainiac\static\images\Te-gl_0011.jpg",
+#     r"C:\Users\Jahnavi\Documents\3rd_Year\Y3S2\[C] CSE301\[0] Brainiac Repo\brainiac\static\images\Te-gl_0012.jpg",
+#     r"C:\Users\Jahnavi\Documents\3rd_Year\Y3S2\[C] CSE301\[0] Brainiac Repo\brainiac\static\images\Te-gl_0013.jpg",
+#     r"C:\Users\Jahnavi\Documents\3rd_Year\Y3S2\[C] CSE301\[0] Brainiac Repo\brainiac\static\images\Te-gl_0014.jpg"
+# ]
+test_img_urls = [
+    r"C:\Users\Jahnavi\Documents\3rd_Year\Y3S2\[C] CSE301\[0] Brainiac Repo\brainiac\static\images\Te-me_0048.jpg",
+    r"C:\Users\Jahnavi\Documents\3rd_Year\Y3S2\[C] CSE301\[0] Brainiac Repo\brainiac\static\images\Te-me_0049.jpg",
+    r"C:\Users\Jahnavi\Documents\3rd_Year\Y3S2\[C] CSE301\[0] Brainiac Repo\brainiac\static\images\Te-me_0050.jpg",
+    r"C:\Users\Jahnavi\Documents\3rd_Year\Y3S2\[C] CSE301\[0] Brainiac Repo\brainiac\static\images\Te-me_0051.jpg",
+    r"C:\Users\Jahnavi\Documents\3rd_Year\Y3S2\[C] CSE301\[0] Brainiac Repo\brainiac\static\images\Te-me_0052.jpg"
+]
 # test_img_urls = [
 #     r"C:\Users\Jahnavi\Documents\3rd_Year\Y3S2\[C] CSE301\[0] Brainiac Repo\brainiac\static\images\Te-pi_0010.jpg",
 #     r"C:\Users\Jahnavi\Documents\3rd_Year\Y3S2\[C] CSE301\[0] Brainiac Repo\brainiac\static\images\Te-pi_0011.jpg",
@@ -15,13 +29,13 @@ model = load_model(model_path)
 #     r"C:\Users\Jahnavi\Documents\3rd_Year\Y3S2\[C] CSE301\[0] Brainiac Repo\brainiac\static\images\Te-pi_0013.jpg",
 #     r"C:\Users\Jahnavi\Documents\3rd_Year\Y3S2\[C] CSE301\[0] Brainiac Repo\brainiac\static\images\Te-pi_0014.jpg"
 # ]
-test_img_urls = [
-    r"C:\Users\Jahnavi\Documents\3rd_Year\Y3S2\[C] CSE301\[0] Brainiac Repo\brainiac\static\images\no 94.jpg",
-    r"C:\Users\Jahnavi\Documents\3rd_Year\Y3S2\[C] CSE301\[0] Brainiac Repo\brainiac\static\images\no 95.jpg",
-    r"C:\Users\Jahnavi\Documents\3rd_Year\Y3S2\[C] CSE301\[0] Brainiac Repo\brainiac\static\images\no 96.jpg",
-    r"C:\Users\Jahnavi\Documents\3rd_Year\Y3S2\[C] CSE301\[0] Brainiac Repo\brainiac\static\images\no 97.jpg",
-    r"C:\Users\Jahnavi\Documents\3rd_Year\Y3S2\[C] CSE301\[0] Brainiac Repo\brainiac\static\images\no 98.jpg",
-]
+# test_img_urls = [
+#     r"C:\Users\Jahnavi\Documents\3rd_Year\Y3S2\[C] CSE301\[0] Brainiac Repo\brainiac\static\images\no 94.jpg",
+#     r"C:\Users\Jahnavi\Documents\3rd_Year\Y3S2\[C] CSE301\[0] Brainiac Repo\brainiac\static\images\no 95.jpg",
+#     r"C:\Users\Jahnavi\Documents\3rd_Year\Y3S2\[C] CSE301\[0] Brainiac Repo\brainiac\static\images\no 96.jpg",
+#     r"C:\Users\Jahnavi\Documents\3rd_Year\Y3S2\[C] CSE301\[0] Brainiac Repo\brainiac\static\images\no 97.jpg",
+#     r"C:\Users\Jahnavi\Documents\3rd_Year\Y3S2\[C] CSE301\[0] Brainiac Repo\brainiac\static\images\no 98.jpg",
+# ]
 
 # Perform prediction
 predictions = []
